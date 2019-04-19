3c966533072e4b1b7f09a9eb6f371152b9557681
huanghuiyu <huanghuiyu@yidian-inc.com>
2019-04-19 11:21:35 +0800

    config logger

--- config/config.prod.js
+++ config/config.prod.js
@@ -15,6 +15,20 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1554273891258_9652';
 
+  config.cluster = {
+    listen: {
+      port: 7001,
+      hostname: '127.0.0.1',
+    }
+  };
+
+  config.logger = {
+    dir: '/var/lib/jenkins/workspace/deploy_penda_node/log',
+    outputJSON: true,
+    level: 'DEBUG',
+    allowDebugAtProd: true,
+  };
+
   // add your middleware config here
   config.middleware = [];
 
